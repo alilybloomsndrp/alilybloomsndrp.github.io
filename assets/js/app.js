bd624197f64ea9ad4c22edca43894b2c1e7f317e
GravyLP <gravaante@outlook.com>
2025-02-16 09:46:05 +0100

    fixes?

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,13 +23,27 @@ const hideValues = {
     });
     target.dataset.hideme = showHide;
   }
-  
-  /* set up event handlers on the buttons */
+
   const options = {
     capture: true
   };
-  /* we do this first to prevent the click from happening */
+
   const toggleButtons = document.querySelectorAll('.toggle-button');
   toggleButtons.forEach(el => {
     el.addEventListener('click', toggleEventHandler, options)
   });
+
+
+const fadeInDivs = document.querySelectorAll('.itemimg');
+const fadeInObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('scale-up');
+      observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.5
+});
+fadeInDivs.forEach(div => fadeInObserver.observe(div));
+
